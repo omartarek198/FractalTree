6ed8a2a64246eeac90a3b1c3dd331e8adef2aebc
omartarek198 <omar.tarek29@msa.edu.eg>
2023-01-24 16:01:19 +0200

    adding project files

---
+++ Fractal Tree/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace Fractal_Tree
+{
+    public partial class Form1 : Form
+    {
+
+        float length;
+        float changeAngleInRad = 0.5f;
+        int depth = 13;
+        Random randomNumbersGenerator = new Random();
+        bool SimulateRandomChange = false;
+        bool InstructionsVisible = true;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.Paint += Form1_Paint;
+
+        }
+
+        private void Form1_Paint(object? sender, PaintEventArgs e)
+        {
+            DrawScene(e.Graphics);
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+            this.WindowState= FormWindowState.Maximized;
+
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void Form1_KeyDown(object? sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Left:
+                    changeAngleInRad -= 0.05f;
+                    break;
+                case Keys.Right:
+                    changeAngleInRad += 0.05f;
+                    break;
+                case Keys.Up:
+                    depth += 1;
+                    break;
+                case Keys.Down:
+                    depth -= 1;
+                    break;
+            }
+
+            DrawScene(CreateGraphics());
+        }
+
+        void branch(Graphics g, PointF point, float angle ,float length)
+
+        {
+
+            Pen p = new Pen(Brushes.White);
+
+
+            if (length <= 0) return;
+            //length = length *0.67f;
+            //double dx = length * Math.Cos(angle);
+            //double dy = length * Math.Sin(angle);
+            //g.DrawLine(Pens.White, point.X, point.Y, point.X + (float)dx, point.Y - (float)dy);
+            ////g.DrawLine(Pens.White, point.X, point.Y, point.X - (float)dx, point.Y - (float)dy);
+
+            //float dA = ((float)Math.PI * 1   / 180);
+            //branch(g, new PointF(point.X + (float)dx, point.Y - (float)dy), length, ((float)Math.PI * 180 / 180) - angle + dA);
+
+
+            //branch(g, new PointF(point.X + (float)dx, point.Y - (float)dy), length, angle + dA);
+
+            ////branch(g, new PointF(point.X - (float)dx, point.Y - (float)dy), length, angle +dA );
+
+            //////branch(g, new pointF(point.x + (float)dx, point.y - (float)dy), length, angle);
+
+            if (length-4 <= 0) p = new Pen(Brushes.Purple); 
+
+
+            int x2 = (int)point.X + (int)(Math.Cos(angle) * length * 10.0);
+            int y2 = (int)point.Y + (int)(Math.Sin(angle) * length * 10.0);
+            g.DrawLine(p, new Point(x2, y2), point ) ;
+
+            float changeValue = (SimulateRandomChange) ?(float) randomNumbersGenerator.Next(0,(int)length) *  (float) randomNumbersGenerator.NextDouble(): 1;
+            branch(g, new Point(x2, y2), angle - changeAngleInRad, (float)Math.Ceiling( (float) length -changeValue));
+            branch(g, new Point(x2, y2), angle + changeAngleInRad, (float)Math.Ceiling( (float) length -changeValue));
+
+
+
+        }
+
+        void DrawInstructions(Graphics g)
+        {
+
+            SolidBrush brush = new SolidBrush(Color.White);
+            g.DrawString("Angle change ≈ " + Math.Round(changeAngleInRad * (180) / (Math.PI)), new Font("Arial", 16), brush, new PointF(10, 10));
+            g.DrawString("Depth = " + depth.ToString() , new Font("Arial", 16), brush, new PointF(10, 50));
+            g.DrawString("Left/Right to control angle"  , new Font("Arial", 16), brush, new PointF(10, 100));
+            g.DrawString("Up/Down to control Depth", new Font("Arial", 16), brush, new PointF(10, 150));
+            g.DrawString("Press U to show/hide controls", new Font("Arial", 16), brush, new PointF(10, 200));
+
+
+        }
+        void DrawScene(Graphics g)
+        {
+            g.Clear(Color.Black);
+
+            DrawInstructions(g);
+
+            //g.DrawLine(Pens.White, this.Width / 2, this.Height, this.Width / 2, 650);
+
+            PointF point= new PointF(this.Width /2, 650);
+
+            float angle = 30;
+            //branch(g, point,length,((float) Math.PI * angle /180));
+            //branch(g, point, length, ((float)Math.PI * 180 / 180) - ((float)Math.PI * angle / 180));
+
+
+            branch(g, new PointF( ClientSize.Width / 2, ClientSize.Height - 10) , (float)-Math.PI/ 2, depth);
+
+            //branch(g, point, length,   0.7f *3.56f);
+
+
+        }
+
+     
+    }
+}
